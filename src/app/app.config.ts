0b8ffd6660daa49b972011ebbb7eb96d3604e27f
Roman Babiak <its.alfinity@gmail.com>
2025-05-02 17:21:27 +0300

    base href

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { DATA, DATA_TOKEN } from './data';
 import { of } from 'rxjs';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    { provide: APP_BASE_HREF, useValue: '/market-test' },
     { provide: LOCALE_ID, useValue: 'uk' },
     {
       provide: DATA_TOKEN,
